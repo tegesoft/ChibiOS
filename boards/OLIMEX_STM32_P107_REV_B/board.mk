e81cd0807a7cd2559d3a571f69ab794ab619590a
Jeremie Delaitre <jeremie.delaitre@tegesoft.com>
2012-10-09 16:16:03 +0200

    Add support for the OLIMEX_STM32_P107_REV_B board (WIP, courtesy of mabl)

---
+++ boards/OLIMEX_STM32_P107_REV_B/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = ${CHIBIOS}/boards/OLIMEX_STM32_P107_REV_B/board.c
+
+# Required include directories
+BOARDINC = ${CHIBIOS}/boards/OLIMEX_STM32_P107_REV_B
