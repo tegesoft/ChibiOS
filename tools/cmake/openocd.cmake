fea8811b8a34a003174342ec0ee6c5763943a35e
Jeremie Delaitre <jeremie.delaitre@tegesoft.com>
2012-10-29 11:07:56 +0100

    Add a add_flash_and_run_target() macro in openocd.cmake

--- tools/cmake/openocd.cmake
+++ tools/cmake/openocd.cmake
@@ -2,33 +2,54 @@
 set(OPENOCD_INTERFACE_CFG "" CACHE FILEPATH "OpenOCD interface configuration file")
 set(OPENOCD_TARGET_CFG "" CACHE FILEPATH "OpenOCD target/board configuration file")
 
-# create a flash target using OpenOCD
-macro(add_flash_target target_to_flash)
+# try to locate OpenOCD
+find_program(
+    OPENOCD_EXE
+    NAMES openocd openocd-0.6.0 openocd.exe openocd-0.6.0.exe
+    PATHS ${OPENOCD_ROOT}
+    DOC "OpenOCD program"
+)
+if(OPENOCD_EXE)
+    message("Found OpenOCD: ${OPENOCD_EXE}.")
+else()
+    message(FATAL_ERROR "OpenOCD not found.")
+endif()
 
-    # try to locate OpenOCD
-    find_program(
-        OPENOCD_EXE
-        NAMES openocd openocd-0.6.0 openocd.exe openocd-0.6.0.exe
-        PATHS ${OPENOCD_ROOT}
-        DOC "OpenOCD program"
+# helper macro to create a target using OpenOCD
+macro(create_openocd_target custom_name target_to_flash)
+    add_custom_target(
+        ${custom_name}
+        ${OPENOCD_EXE}
+        -f ${OPENOCD_INTERFACE_CFG}
+        -f ${OPENOCD_TARGET_CFG}
+        ${ARGN}
+        DEPENDS ${target_to_flash}
     )
-    if(OPENOCD_EXE)
-        message(STATUS "Found OpenOCD: ${OPENOCD_EXE}.")
-
-        # add the flash target
-        get_property(TARGET_LOCATION TARGET ${target_to_flash} PROPERTY LOCATION)
-        set(OPENOCD_ARGS
-            -f ${OPENOCD_INTERFACE_CFG}
-            -f ${OPENOCD_TARGET_CFG}
-            -c \"init\"
-            -c \"reset init\"
-            -c \"flash write_image erase ${TARGET_LOCATION}\"
-            -c \"shutdown\"
-        )
-        add_custom_target(flash-${target_to_flash} ${OPENOCD_EXE} ${OPENOCD_ARGS} DEPENDS ${target_to_flash})
-    else()
-        message(STATUS "OpenOCD not found, no flash target will be created for ${target_to_flash} (add the OPENOCD_ROOT variable if OpenOCD is not in a default path).")
-    endif()
-
 endmacro()
 
+# create a flash-xxx target
+macro(add_flash_target target_to_flash)
+    get_property(TARGET_LOCATION TARGET ${target_to_flash} PROPERTY LOCATION)
+    create_openocd_target(
+        flash-${target_to_flash}
+        ${target_to_flash}
+        -c \"init\"
+        -c \"reset init\"
+        -c \"flash write_image erase ${TARGET_LOCATION}\"
+        -c \"shutdown\"
+    )
+endmacro()
+
+# create a flash-and-run-xxx target
+macro(add_flash_and_run_target target_to_flash)
+    get_property(TARGET_LOCATION TARGET ${target_to_flash} PROPERTY LOCATION)
+    create_openocd_target(
+        flash-and-run-${target_to_flash}
+        ${target_to_flash}
+        -c \"init\"
+        -c \"reset init\"
+        -c \"flash write_image erase ${TARGET_LOCATION}\"
+        -c \"reset\"
+        -c \"shutdown\"
+    )
+endmacro()
