08fdbaf35ea608ca3e9148c00096378c6d9e4981
Laurent Gomila <laurent.gom@tegesoft.com>
2014-03-25 11:02:35 +0100

    Some fixes required after update

--- tools/cmake/ext/chprintf.cmake
+++ tools/cmake/ext/chprintf.cmake
@@ -4,5 +4,6 @@ set(CHIBIOS_SRC
     ${CHIBIOS_SRC}
     ${CHIBIOS_ROOT}/os/various/chprintf.h
     ${CHIBIOS_ROOT}/os/various/chprintf.c
+    ${CHIBIOS_ROOT}/os/various/memstreams.h
+    ${CHIBIOS_ROOT}/os/various/memstreams.c
 )
-
